f85885d55ef22c90a1875597a0d55d3e39084e2a
Serhii <s1712@tmm-sapr.org>
2020-02-08 11:11:44 +0200

    Init Commit

---
+++ PHPWebProject1/index.php
@@ -0,0 +1,16 @@
+
+<h1 class="hello">Test</h1>
+
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
+<script>
+	$(document).ready(function () {
+
+		
+    });
+</script>
+
+<style>
+	.hilite {
+	    margin-top: 1000px;
+    }
+</style>
