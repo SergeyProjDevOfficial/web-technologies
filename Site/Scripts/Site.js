959c373097dc3f367a6ec18efc0e5f27c70140a4
Serhii <s1712@tmm-sapr.org>
2020-02-09 20:50:59 +0200

    Full index page

--- Site/Scripts/Site.js
+++ Site/Scripts/Site.js
@@ -1,4 +1,13 @@
 ﻿$(document).ready(function () {
 
-    
+    // partials
+    $('.render-header').load('../Partials/header.html');
+    $('.render-footer').load('../Partials/footer.html');
+    $('.render-contacts').load('../Partials/contactsBar.html');
+    $('.render-table').load('../Partials/sortableTable.html');
+    $('.render-films-list').load('../Partials/filmsList.html');
+    $('.render-words-block').load('../Partials/wordsBlock.html');
+    $('.render-photos-block').load('../Partials/photosBlock.html');
+    $('.render-quote').load('../Partials/quote.html');
+    $('.render-map-with-text').load('../Partials/mapBlock.html');
 });
