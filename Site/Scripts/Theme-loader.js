842a1affcd3081d211dda1e82efe3469129a64d8
Serhij <56254988+SergeyProjDevOfficial@users.noreply.github.com>
2020-02-29 13:12:54 +0200

    Global styles replacment

---
+++ Site/Scripts/Theme-loader.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+        try {
+            var $theme = $('#theme');
+            var $cookie = $.cookie('theme');
+            
+            if ($cookie === 'light') {
+                    $theme.attr('href', '../Styles/theme-light.css');
+            }
+            if ($cookie === 'dark') {
+                    $theme.attr('href', '../Styles/theme-dark.css');
+            }
+        } catch {}
+});
